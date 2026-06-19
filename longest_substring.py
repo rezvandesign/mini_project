b79a1a704b408f5ffe6266c9a2327371963d3f65
rezvan <moradiirezvan@gmail.com>
2026-06-19 17:13:37 +0330

    add longest_substring.py

---
+++ longest_substring.py
@@ -0,0 +1,19 @@
+def length_of_longest_substring(s):
+    char_index = {}   
+    left = 0          
+    max_length = 0
+
+    for right in range(len(s)):
+        char = s[right]
+
+        
+        if char in char_index and char_index[char] >= left:
+            left = char_index[char] + 1
+
+        
+        char_index[char] = right
+
+        
+        max_length = max(max_length, right - left + 1)
+
+    return max_length
